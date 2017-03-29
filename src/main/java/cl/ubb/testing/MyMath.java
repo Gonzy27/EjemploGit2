6b46b09074537836bc18eddf2a960d55fc12a585
Gonzalo Espinoza <goespin@alumnos.ubiobio.cl>
2017-03-29 12:16:04 -0300

    Primer commit

---
+++ src/main/java/cl/ubb/testing/MyMath.java
@@ -0,0 +1,8 @@
+package cl.ubb.testing;
+
+public class MyMath {
+	
+	public int add(int a, int b){
+		return a+b;
+	}
+}
---
+++ src/test/java/cl/ubb/testing/TestMyMath.java
@@ -0,0 +1,18 @@
+package cl.ubb.testing;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestMyMath {
+	
+	@Test
+	public void AddTwoAndTwoIsFour(){
+		//Arrange
+		MyMath myMath = new MyMath();
+		int resultado;
+		//Act
+		resultado = myMath.add(2,2);
+		//Assert
+		assertEquals(resultado,4);
+	}
+}
